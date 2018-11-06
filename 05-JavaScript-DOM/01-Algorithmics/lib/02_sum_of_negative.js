671efd4c2662a882d633fd7667d2d0be56d3c114
Marine Chollet <marine.chollet@gmail.com>
2018-11-06 12:35:05 +0800

    sumofnegative

--- 05-JavaScript-DOM/01-Algorithmics/lib/02_sum_of_negative.js
+++ 05-JavaScript-DOM/01-Algorithmics/lib/02_sum_of_negative.js
@@ -1,5 +1,23 @@
 function sumOfNegative(numbers) {
   // TODO: You are getting an array `numbers`. Return the sum of **negative** numbers only.
+
+//   let sum = 0;
+//   for(let i = 0; i <= numbers.length; i++){
+//     if(numbers[i] < 0)
+//     {
+//       sum += numbers[i];
+//     }
+//   }
+//   return sum;
+// }
+
+let sum = 0;
+numbers.forEach((number) => {
+  if (number < 0) sum += number;
+});
+return sum;
 }
+
+
 
 module.exports = sumOfNegative; // Do not remove.
